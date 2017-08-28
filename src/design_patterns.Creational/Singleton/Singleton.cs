3fd7af020b1c23ab4ba13e67253388b1f675761c
igorvinnicius <igorvinnicius@gmail.com>
2017-08-28 17:31:21 -0300

    Singleton Pattern

---
+++ src/design_patterns.Creational/Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace design_patterns.Creational.Singleton
+{
+    public class Singleton
+    {
+		private static Singleton _instance;
+
+		private Singleton()	{ }
+
+		public static Singleton Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new Singleton();
+				}
+
+				return _instance;
+			}
+		}
+
+    }
+}
