a6b3013f28d0c478b64dc8ea213cbbb6245faedd
igorvinnicius <igorvinnicius@gmail.com>
2017-08-29 11:30:14 -0300

    Adapter Pattern

---
+++ src/design_patterns.Structural/Adapter/OpenGLImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace design_patterns.Structural.Adapter
+{
+    public class OpenGLImage
+    {
+
+		public void LoadImage(string path)
+		{
+			// Load image
+		}
+
+		public void DrawImage(int posX, int posY)
+		{
+
+		}
+
+    }
+}
---
+++ src/design_patterns.Structural/Adapter/OpenGLImageAdapter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace design_patterns.Structural.Adapter
+{
+    class OpenGLImageAdapter
+    {
+    }
+}
