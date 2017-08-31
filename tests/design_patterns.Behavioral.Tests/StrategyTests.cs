4a6f044335faba3b55d33c57a1ec71dd35cb999a
igorvinnicius <igorvinnicius@gmail.com>
2017-08-30 21:58:28 -0300

    Strategy Pattern Tests

---
+++ tests/design_patterns.Behavioral.Tests/StrategyTests.cs
@@ -0,0 +1,43 @@
+﻿using design_patterns.Behavioral.Strategy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace design_patterns.Behavioral.Tests
+{
+    public class StrategyTests
+    {
+
+		[Fact]
+		public void Should_Return_15_When_ShippingCalculationStrategyOne()
+		{
+			// Arrange
+			var strategy = new ShippingCalculationStrategyOne();
+			var service = new ShippingCostService(strategy);
+			var order = new Order();
+			
+			// Act
+			var cost = service.CalculateShippingCost(order);
+
+			// Assert
+			Assert.Equal(15, cost);
+		}
+
+		[Fact]
+		public void Should_Return_30_When_ShippingCalculationStrategyTwo()
+		{
+			// Arrange
+			var strategy = new ShippingCalculationStrategyTwo();
+			var service = new ShippingCostService(strategy);
+			var order = new Order();
+			
+			// Act
+			var cost = service.CalculateShippingCost(order);
+
+			// Assert
+			Assert.Equal(30, cost);
+		}
+
+	}
+}
